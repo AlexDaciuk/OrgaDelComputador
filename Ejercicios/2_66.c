ce021784ad2f9eb3d13cf51376553038e772904a
Alexis Daciuk <adaciuk@gmail.com>
2019-08-26 08:12:34 -0300

    Empiezo

---
+++ Ejercicios/2_66.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stint.h>
+
+/*
+    Devolver la posicion del bit mas significativo que es 1
+
+   0xFF00 -> 11111111 00000000 -- devolver --> 10000000 00000000
+   0x6600 -> 01100110 00000000 -- devolver --> 01000000 00000000
+
+ */
+
+
+int main(int argc, char const *argv[]) {
+
+        uint32_t leftmost_one(uint32_t val){
+
+
+        }
+
+        return 0;
+}
