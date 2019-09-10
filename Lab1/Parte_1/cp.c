18c3c8bc1c2aa47f1640a95b44743ebafa622fab
Alexis Daciuk <adaciuk@gmail.com>
2019-09-10 14:47:03 -0300

    Funciona a medias, me tira basura al final del archivo, escribe un byte de mas

--- Lab1/Parte_1/cp.c
+++ Lab1/Parte_1/cp.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <sysexits.h>
-#ifndef BUFF_SIZE
-#define BUFF_SIZE 10
+#define BUFF_SIZE 5
 
 int main(int argc, char const *argv[]) {
         /*
@@ -10,7 +9,7 @@ int main(int argc, char const *argv[]) {
            El segundo argumento es el source y el tercero el destination
          */
         if (argc < 3) {
-                return EX_USAGE;
+                return EX_USAGE; // Codigo 64
         }
 
         FILE *sptr, *dptr;
@@ -19,18 +18,22 @@ int main(int argc, char const *argv[]) {
         sptr = fopen(argv[1], "rb");
         if (!sptr) {
                 printf("No se pudo abrir fichero de origen.\n");
-                return EX_NOINPUT;
+                return EX_NOINPUT; // Codigo 66
         }
 
         dptr = fopen(argv[2], "wb");
         if (!dptr) {
                 printf("No se pudo abrir fichero de destino\n");
-                return EX_CANTCREAT;
+                return EX_CANTCREAT; // Codigo 73
         }
 
         while(!feof(sptr)) {
                 /* Trato de leer 10 bytes */
                 int buff_read = fread(buffer, sizeof(buffer), 1, sptr);
+                printf("Lei %i paquetes de %i bytes del archivo.\n", buff_read, BUFF_SIZE);
+
+                int buff_write = fwrite(buffer, sizeof(buffer), 1, dptr);
+                printf("Escribi %i paquetes de %i bytes al archivo.\n", buff_write, BUFF_SIZE );
         }
         return EX_OK;
 }
