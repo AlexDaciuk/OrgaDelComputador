8ccb696a668df68be1b59ba0ab8988a01b712ae0
Alexis Daciuk <adaciuk@gmail.com>
2019-09-10 14:20:30 -0300

    Le paso uncrustify

--- Lab1/Parte_1/ls.c
+++ Lab1/Parte_1/ls.c
@@ -4,23 +4,23 @@
 #include <linux/limits.h>
 
 int main(int argc, char const *argv[]) {
-  /* Chequeo cantidad de argumentos */
-  if (argc > 1) {
-    return 22;
-  }
+        /* Chequeo cantidad de argumentos */
+        if (argc > 1) {
+                return 22;
+        }
 
-  /* Aprovecho PATH_MAX que esta en limits.h */
-  char cwd[PATH_MAX];
-  getcwd(cwd, sizeof(cwd));
+        /* Aprovecho PATH_MAX que esta en limits.h */
+        char cwd[PATH_MAX];
+        getcwd(cwd, sizeof(cwd));
 
-  printf("El directorio es %s\n \n", cwd);
+        printf("El directorio es %s\n \n", cwd);
 
-  struct dirent *dp;
-  DIR *dir = opendir(cwd);
+        struct dirent *dp;
+        DIR *dir = opendir(cwd);
 
-  while ( (dp = readdir(dir)) ){
-    printf("%s  \n", dp->d_name);
-  }
+        while ( (dp = readdir(dir)) ) {
+                printf("%s  \n", dp->d_name);
+        }
 
-  return 0;
+        return 0;
 }
