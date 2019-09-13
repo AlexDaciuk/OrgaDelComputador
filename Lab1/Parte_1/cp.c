9fc0c8c8014b7347ef95541518f8cd7a8d48987a
Alexis Daciuk <adaciuk@gmail.com>
2019-09-13 16:57:50 -0300

    Va queriendo, hace falta agregar un par de comprobaciones mas que piden en el enunciado

--- Lab1/Parte_1/cp.c
+++ Lab1/Parte_1/cp.c
@@ -1,46 +1,60 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
+#include <errno.h>
 #include <sysexits.h>
 #define BUFF_SIZE 10
 
 int main(int argc, char const *argv[]) {
-        /*
-           Chequeo que hayan pasado al menos 2 argumentos, source y destination
-           El primer argumento siempre es el comando de ejecucion del programa
-           El segundo argumento es el source y el tercero el destination
-         */
-        if (argc < 3) {
-                return EX_USAGE; // Codigo 64
-        }
+				/*
+				   Chequeo que hayan pasado al menos 2 argumentos, source y destination
+				   El primer argumento siempre es el comando de ejecucion del programa
+				   El segundo argumento es el source y el tercero el destination
+				 */
+				if (argc < 3) {
+								return EX_USAGE; // Codigo 64
+				}
 
-        FILE *sptr, *dptr;
-        unsigned char buffer[BUFF_SIZE];
-        int buff_read = 0;
+				int r_fd, w_fd;
+				unsigned char buffer[BUFF_SIZE];
+				int buff_read;
 
-        sptr = fopen(argv[1], "rb");
-        if (!sptr) {
-                printf("No se pudo abrir fichero de origen.\n");
-                return EX_NOINPUT; // Codigo 66
-        }
+				r_fd = open(argv[1], O_RDONLY);
+				if (r_fd < 0) {
+								printf("No se pudo abrir fichero de origen.\n");
+								printf("Errno : %i \n", errno);
+								return EX_NOINPUT; // Codigo 66
+				}
 
-        dptr = fopen(argv[2], "wb");
-        if (!dptr) {
-                printf("No se pudo abrir fichero de destino\n");
-                return EX_CANTCREAT; // Codigo 73
-        }
+				w_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT,
+				            S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+				if (w_fd < 0) {
+								printf("No se pudo abrir fichero de destino\n");
+								printf("Errno : %i \n", errno);
+								return EX_CANTCREAT; // Codigo 73
+				}
 
-        while( (buff_read = fread(buffer, sizeof(char), 10, sptr)) ) {
-                printf("Lei %i paquetes de %i bytes.\n", buff_read, BUFF_SIZE);
+				while( ( (buff_read = read(r_fd, buffer, BUFF_SIZE)) > 0) )   {
+								printf("Lei  %i bytes.\n", buff_read);
 
-                int total_wrote = 0;
+								int total_wrote = 0;
 
-                while ((buff_read - total_wrote) != 0) {
-                        int buff_write = fwrite(buffer, sizeof(char), buff_read, dptr);
+								while ((buff_read - total_wrote) > 0) {
+												int buff_write = write(w_fd, buffer, buff_read);
 
-                        total_wrote += buff_write;
+												if (buff_write > 0) {
+																total_wrote += buff_write;
 
-                        printf("Escribi %i paquetes de %i bytes.\n", buff_write, BUFF_SIZE );
-                }
-        }
+																printf("Escribi %i bytes.\n", buff_write);
+												}
+								}
+				}
 
-        return EX_OK;
+				printf("Errno : %i\n", errno );
+
+				return EX_OK;
 }
