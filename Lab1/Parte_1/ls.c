1a93ecf017c6671c6b6c30cea4558820f04ae757
Alexis Daciuk <adaciuk@gmail.com>
2019-09-13 14:34:52 -0300

    Estoy tratando de usar fstatat pero no estaria queriendo

--- Lab1/Parte_1/ls.c
+++ Lab1/Parte_1/ls.c
@@ -1,27 +1,45 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <sysexits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <linux/limits.h>
+#include <string.h>
+
+
 
 int main(int argc, char const *argv[]) {
-        /* Chequeo cantidad de argumentos */
-        if (argc > 1) {
-                return EX_USAGE; // Codigo 64
-        }
+				/* Aprovecho PATH_MAX que esta en limits.h */
+				char cwd[PATH_MAX];
 
-        /* Aprovecho PATH_MAX que esta en limits.h */
-        char cwd[PATH_MAX];
-        getcwd(cwd, sizeof(cwd));
+				/* Chequeo cantidad de argumentos */
+				if (argc == 1) {
+								getcwd(cwd, sizeof(cwd));
+				} else if (argc == 2) {
+								*cwd = *argv[1];
+				} else {
+								return EX_USAGE;
+				}
 
-        printf("El directorio es %s\n \n", cwd);
+				printf("El directorio es %s\n \n", cwd);
 
-        struct dirent *dp;
-        DIR *dir = opendir(cwd);
+				struct dirent *dp;
+				int dir_fd;
+				struct stat *fileStat;
+				DIR *dir = opendir(cwd);
+				dir_fd = dirfd(dir);
 
-        while ( (dp = readdir(dir)) ) {
-                printf("%s  \n", dp->d_name);
-        }
+				while ( (dp = readdir(dir)) ) {
+								printf("\nDirfd: %i, name: %s\n", dir_fd, dp->d_name);
+								if (fstatat(dir_fd, dp->d_name, fileStat, 0) != -1) {
+												printf("%s \t %li", dp->d_name, fileStat->st_size);
+								}
+								printf("Errno %i\n", errno);
+				}
 
-        return EX_OK;
+				return EX_OK;
 }
