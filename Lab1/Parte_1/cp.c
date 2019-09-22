5a81199b6ab9760a1c0a69e2111efdc6aa02dedd
Alexis Daciuk <adaciuk@gmail.com>
2019-09-22 13:51:05 -0300

    Arreglo un toque el codigo y agrego chequeo de errores que piden en el enunciado, tengo que mejorar el ciclo que copia las cosas para agregar el manejo de las señales

--- Lab1/Parte_1/cp.c
+++ Lab1/Parte_1/cp.c
@@ -8,14 +8,15 @@
 #include <errno.h>
 #include <sysexits.h>
 #define BUFF_SIZE 10
+#define DEF_PERM S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
 
 int main(int argc, char const *argv[]) {
 				/*
-				   Chequeo que hayan pasado al menos 2 argumentos, source y destination
-				   El primer argumento siempre es el comando de ejecucion del programa
-				   El segundo argumento es el source y el tercero el destination
+				   Chequeo que hayan pasado al menos 2 argumentos, primer argumento siempre es el comando
+				   de ejecucion del programa, el segundo es el source y el tercero el destination.
 				 */
-				if (argc < 3) {
+				if (argc != 3) {
+								printf("Cantidad de argumentos incorrecta, se necesitan 2 argumentos para utilizar este programa.\n", );
 								return EX_USAGE; // Codigo 64
 				}
 
@@ -23,33 +24,35 @@ int main(int argc, char const *argv[]) {
 				unsigned char buffer[BUFF_SIZE];
 				int buff_read;
 
-				r_fd = open(argv[1], O_RDONLY);
-				if (r_fd < 0) {
-								printf("No se pudo abrir fichero de origen.\n");
-								printf("Errno : %i \n", errno);
+				if ( (r_fd = open(argv[1], O_RDONLY)) ) {
+								if (errno == 21) {
+												printf("El archivo %s es un directorio.\n", *argv[1]);
+								}
+								printf("No se pudo abrir fichero de origen, error: %m\n");
 								return EX_NOINPUT; // Codigo 66
 				}
 
-				w_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT,
-				            S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-				if (w_fd < 0) {
-								printf("No se pudo abrir fichero de destino\n");
-								printf("Errno : %i \n", errno);
+				// Abro el archivo de destino, si no existe, lo creo con permisos 0666
+				// si existe, lo trunco
+				if ( (w_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, DEF_PERM)) ) {
+								printf("No se pudo abrir fichero de destino, error: %m\n");
 								return EX_CANTCREAT; // Codigo 73
 				}
 
 				while( ( (buff_read = read(r_fd, buffer, BUFF_SIZE)) > 0) )   {
-								printf("Lei  %i bytes.\n", buff_read);
+								if (errno < 0) {
+												printf("Lei  %i bytes.\n", buff_read);
 
-								int total_wrote = 0;
+												int total_wrote = 0;
 
-								while ((buff_read - total_wrote) > 0) {
-												int buff_write = write(w_fd, buffer, buff_read);
+												while ((buff_read - total_wrote) > 0) {
+																int buff_write = write(w_fd, buffer, buff_read);
 
-												if (buff_write > 0) {
-																total_wrote += buff_write;
+																if (buff_write > 0) {
+																				total_wrote += buff_write;
 
-																printf("Escribi %i bytes.\n", buff_write);
+																				printf("Escribi %i bytes.\n", buff_write);
+																}
 												}
 								}
 				}
